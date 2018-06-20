56bbe7604d663bc849dd11c1b7ae39062f0fb8ea
Sergei Galkovskii <sergei.galkovskii@Sergeis-MacBook-Pro.local>
2018-06-20 10:03:35 +0300

    Updated buttons styles

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -42,14 +42,14 @@ class Home extends React.Component {
                 <Col xs={12} className="mb-3">
                     {randomPhoto(car)}
                 </Col>
-                <Col xs={12} className="d-flex flex-wrap px-0">
-                    <Col xs={4}>
+                <Col xs={12} className="d-flex flex-wrap px-2">
+                    <Col xs={12} md={4} className="p-2">
                         <Button color="success" className="w-100" onClick={() => this.onVoting(true, car.id)}>Love it</Button>
                     </Col>
-                    <Col xs={4}>
+                    <Col xs={12} md={4} className="p-2">
                         <Button color="danger" className="w-100" onClick={() => this.onVoting(false, car.id)}>Hate it</Button>
                     </Col>
-                    <Col xs={4} onClick={() => this.props.changePage()}>
+                    <Col xs={12} md={4} className="p-2">
                         <Button color="primary" className="w-100" onClick={changePage}>Show score</Button>
                     </Col>
                 </Col>
